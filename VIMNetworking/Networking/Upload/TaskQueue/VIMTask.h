d66e4210ded4ca39b29b60bda82e023b0c77cec1
Mark Essel <messel@gmail.com>
2015-08-06 16:21:42 -0400

    nonnull makes sense

--- VIMNetworking/Networking/Upload/TaskQueue/VIMTask.h
+++ VIMNetworking/Networking/Upload/TaskQueue/VIMTask.h
@@ -51,7 +51,7 @@ typedef NS_ENUM(NSInteger, VIMTaskState)
 
 @required
 - (void)taskDidComplete:(nonnull VIMTask *)task;
-- (nullable dispatch_queue_t)tasksQueue;
+- (nonnull dispatch_queue_t)tasksQueue;
 
 @end
 
